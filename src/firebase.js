d62b3b24f3391aba4c1ea7de5787eada9b508b9f
valsamosbabis <valsamosbabis@gmail.com>
2026-05-30 13:01:33 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3c6IJFYvKUY6lgyD2IupqtgO6CV_Is1s",
-  authDomain: "vacation-3bc4d.firebaseapp.com",
-  projectId: "vacation-3bc4d",
-  storageBucket: "vacation-3bc4d.firebasestorage.app",
-  messagingSenderId: "484787013070",
-  appId: "1:484787013070:web:4b5142240faa554e43979f"
+  VITE_FIREBASE_apiKey: "AIzaSyC3c6IJFYvKUY6lgyD2IupqtgO6CV_Is1s",
+  VITE_FIREBASE_authDomain: "vacation-3bc4d.firebaseapp.com",
+  VITE_FIREBASE_projectId: "vacation-3bc4d",
+  VITE_FIREBASE_storageBucket: "vacation-3bc4d.firebasestorage.app",
+  VITE_FIREBASE_messagingSenderId: "484787013070",
+  VITE_FIREBASE_appId: "1:484787013070:web:4b5142240faa554e43979f"
 };
 
 const app = initializeApp(firebaseConfig);
